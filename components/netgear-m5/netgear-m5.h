f45e812b13c23052c7158b18394ae0cd822ad6d4
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-08-18 16:25:17 +1000

    a

--- components/netgear-m5/netgear-m5.h
+++ components/netgear-m5/netgear-m5.h
@@ -52,6 +52,14 @@ class NetgearM5Component : public Component {
                      const std::string &body,
                      const std::string &content_type,
                      std::string &response);
+  esp_err_t NetgearM5Component::_request_with_redirects(
+    const std::string &url,
+    esp_http_client_method_t method,
+    const std::string &body,
+    const std::string &content_type,
+    std::string &response,
+    int max_redirects)
+{
 
   static esp_err_t _event_handler(esp_http_client_event_t *evt);
 
