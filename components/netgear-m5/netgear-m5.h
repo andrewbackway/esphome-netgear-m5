5369874748c5cc39c12e07f7e4ebc3d9214a2c00
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2025-08-21 15:54:09 +1000

    a

--- components/netgear-m5/netgear-m5.h
+++ components/netgear-m5/netgear-m5.h
@@ -95,15 +95,10 @@ class NetgearM5Component : public Component {
   };
 
   int clamp01(int v, int lo, int hi);
-
   int bars_from_rsrp(float rsrp_dbm);
-
   int quality_adjust_from_rsrq(float rsrq_db);
-
   int quality_adjust_from_sinr(float sinr_db);
-
   int bars_from_rssi(float rssi_dbm);
-
   int calc_mobile_bars(bool has_rsrp, float rsrp_dbm, bool has_rsrq,
                        float rsrq_db, bool has_sinr, float sinr_db,
                        bool has_rssi, float rssi_dbm);
